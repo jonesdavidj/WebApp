1485e78f911a543d53e361df298657336ce9b176
David Jones <davidjones.home@googlemail.com>
2018-12-14 16:59:20 +0000

    First version complete locally.

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Just a little more than a Hello World...but not much.");
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+console.log("Server running at http://localhost:%d", port);
